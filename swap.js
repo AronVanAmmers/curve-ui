001f67863b44739df5ce77d1252b22e2d45e44df
Michael Egorov <michwill@gmail.com>
2019-10-02 20:10:21 -0700

    UI - selection and getting prices

---
+++ swap.js
@@ -0,0 +1,878 @@
+/*
+In [4]: coins[0]
+Out[4]: '0x9f78Bc79a5A84F5bD3B9E90eFb786829f72fc66c'
+
+In [5]: coins[1]
+Out[5]: '0xBFeC45dbBAE7F403289eA2a0e39D9B06480D2349'
+
+In [6]: coins[2]
+Out[6]: '0xc88fE79b99be819D6FA9512365f418CD73cdBdD8'
+
+In [7]: swap.address
+Out[7]: '0x766d7Ff23546C86780913DeA691bce1fF627CE6B'
+
+In [8]: token.address
+Out[8]: '0x848c28C2243cd78f8d586F93d036b57D4DA892C0'
+*
+ */
+
+var N_COINS = 3;
+var coins = new Array(N_COINS);
+var swap_address = "0x766d7Ff23546C86780913DeA691bce1fF627CE6B";
+var token_address = "0x848c28C2243cd78f8d586F93d036b57D4DA892C0";
+
+var swap_abi = [
+ {
+  "name": "TokenExchange",
+  "inputs": [
+   {
+    "type": "address",
+    "name": "buyer",
+    "indexed": true
+   },
+   {
+    "type": "int128",
+    "name": "sold_id",
+    "indexed": false
+   },
+   {
+    "type": "uint256",
+    "name": "tokens_sold",
+    "indexed": false
+   },
+   {
+    "type": "int128",
+    "name": "bought_id",
+    "indexed": false
+   },
+   {
+    "type": "uint256",
+    "name": "tokens_bought",
+    "indexed": false
+   },
+   {
+    "type": "uint256",
+    "name": "fee",
+    "indexed": false
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "name": "AddLiquidity",
+  "inputs": [
+   {
+    "type": "address",
+    "name": "provider",
+    "indexed": true
+   },
+   {
+    "type": "uint256[3]",
+    "name": "token_amounts",
+    "indexed": false
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "name": "RemoveLiquidity",
+  "inputs": [
+   {
+    "type": "address",
+    "name": "provider",
+    "indexed": true
+   },
+   {
+    "type": "uint256[3]",
+    "name": "token_amounts",
+    "indexed": false
+   },
+   {
+    "type": "uint256[3]",
+    "name": "fees",
+    "indexed": false
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "name": "CommitNewAdmin",
+  "inputs": [
+   {
+    "type": "uint256",
+    "name": "deadline",
+    "indexed": true,
+    "unit": "sec"
+   },
+   {
+    "type": "address",
+    "name": "admin",
+    "indexed": true
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "name": "NewAdmin",
+  "inputs": [
+   {
+    "type": "address",
+    "name": "admin",
+    "indexed": true
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "name": "CommitNewParameters",
+  "inputs": [
+   {
+    "type": "uint256",
+    "name": "deadline",
+    "indexed": true,
+    "unit": "sec"
+   },
+   {
+    "type": "int128",
+    "name": "A",
+    "indexed": false
+   },
+   {
+    "type": "int128",
+    "name": "fee",
+    "indexed": false
+   },
+   {
+    "type": "int128",
+    "name": "admin_fee",
+    "indexed": false
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "name": "NewParameters",
+  "inputs": [
+   {
+    "type": "int128",
+    "name": "A",
+    "indexed": false
+   },
+   {
+    "type": "int128",
+    "name": "fee",
+    "indexed": false
+   },
+   {
+    "type": "int128",
+    "name": "admin_fee",
+    "indexed": false
+   }
+  ],
+  "anonymous": false,
+  "type": "event"
+ },
+ {
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "address[3]",
+    "name": "_coins"
+   },
+   {
+    "type": "address",
+    "name": "_pool_token"
+   },
+   {
+    "type": "int128",
+    "name": "_A"
+   },
+   {
+    "type": "int128",
+    "name": "_fee"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "constructor"
+ },
+ {
+  "name": "add_liquidity",
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "uint256[3]",
+    "name": "amounts"
+   },
+   {
+    "type": "uint256",
+    "unit": "sec",
+    "name": "deadline"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 10061853
+ },
+ {
+  "name": "get_dy",
+  "outputs": [
+   {
+    "type": "uint256",
+    "name": "out"
+   }
+  ],
+  "inputs": [
+   {
+    "type": "int128",
+    "name": "i"
+   },
+   {
+    "type": "int128",
+    "name": "j"
+   },
+   {
+    "type": "uint256",
+    "name": "dx"
+   }
+  ],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 11131745
+ },
+ {
+  "name": "exchange",
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "int128",
+    "name": "i"
+   },
+   {
+    "type": "int128",
+    "name": "j"
+   },
+   {
+    "type": "uint256",
+    "name": "dx"
+   },
+   {
+    "type": "uint256",
+    "name": "min_dy"
+   },
+   {
+    "type": "uint256",
+    "unit": "sec",
+    "name": "deadline"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 11269726
+ },
+ {
+  "name": "remove_liquidity",
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "uint256",
+    "name": "_amount"
+   },
+   {
+    "type": "uint256",
+    "unit": "sec",
+    "name": "deadline"
+   },
+   {
+    "type": "uint256[3]",
+    "name": "min_amounts"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 189600
+ },
+ {
+  "name": "remove_liquidity_imbalance",
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "uint256[3]",
+    "name": "amounts"
+   },
+   {
+    "type": "uint256",
+    "unit": "sec",
+    "name": "deadline"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 10171784
+ },
+ {
+  "name": "commit_new_parameters",
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "int128",
+    "name": "amplification"
+   },
+   {
+    "type": "int128",
+    "name": "new_fee"
+   },
+   {
+    "type": "int128",
+    "name": "new_admin_fee"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 144575
+ },
+ {
+  "name": "apply_new_parameters",
+  "outputs": [],
+  "inputs": [],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 129552
+ },
+ {
+  "name": "revert_new_parameters",
+  "outputs": [],
+  "inputs": [],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 20875
+ },
+ {
+  "name": "commit_transfer_ownership",
+  "outputs": [],
+  "inputs": [
+   {
+    "type": "address",
+    "name": "_owner"
+   }
+  ],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 72920
+ },
+ {
+  "name": "apply_transfer_ownership",
+  "outputs": [],
+  "inputs": [],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 57808
+ },
+ {
+  "name": "revert_transfer_ownership",
+  "outputs": [],
+  "inputs": [],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 20965
+ },
+ {
+  "name": "withdraw_admin_fees",
+  "outputs": [],
+  "inputs": [],
+  "constant": false,
+  "payable": false,
+  "type": "function",
+  "gas": 13770
+ },
+ {
+  "name": "coins",
+  "outputs": [
+   {
+    "type": "address",
+    "name": "out"
+   }
+  ],
+  "inputs": [
+   {
+    "type": "int128",
+    "name": "arg0"
+   }
+  ],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1170
+ },
+ {
+  "name": "balances",
+  "outputs": [
+   {
+    "type": "uint256",
+    "name": "out"
+   }
+  ],
+  "inputs": [
+   {
+    "type": "int128",
+    "name": "arg0"
+   }
+  ],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1200
+ },
+ {
+  "name": "A",
+  "outputs": [
+   {
+    "type": "int128",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1031
+ },
+ {
+  "name": "fee",
+  "outputs": [
+   {
+    "type": "int128",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1061
+ },
+ {
+  "name": "admin_fee",
+  "outputs": [
+   {
+    "type": "int128",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1091
+ },
+ {
+  "name": "owner",
+  "outputs": [
+   {
+    "type": "address",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1121
+ },
+ {
+  "name": "admin_actions_deadline",
+  "outputs": [
+   {
+    "type": "uint256",
+    "unit": "sec",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1151
+ },
+ {
+  "name": "transfer_ownership_deadline",
+  "outputs": [
+   {
+    "type": "uint256",
+    "unit": "sec",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1181
+ },
+ {
+  "name": "future_A",
+  "outputs": [
+   {
+    "type": "int128",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1211
+ },
+ {
+  "name": "future_fee",
+  "outputs": [
+   {
+    "type": "int128",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1241
+ },
+ {
+  "name": "future_admin_fee",
+  "outputs": [
+   {
+    "type": "int128",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1271
+ },
+ {
+  "name": "future_owner",
+  "outputs": [
+   {
+    "type": "address",
+    "name": "out"
+   }
+  ],
+  "inputs": [],
+  "constant": true,
+  "payable": false,
+  "type": "function",
+  "gas": 1301
+ }
+];
+var ERC20_abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+];
+var swap;
+var swap_token;
+var ERC20Contract;
+var from_currency;
+var to_currency
+
+function set_amount(i, el_id) {
+    coins[i].balanceOf(web3.eth.accounts[0], (err, value) => {
+        var out = (value / 1e18).toFixed(18);
+        $(el_id).val(out);
+    });
+}
+
+function init_contracts() {
+    var SwapContract = web3.eth.contract(swap_abi);
+    ERC20Contract = web3.eth.contract(ERC20_abi);
+
+    swap = SwapContract.at(swap_address);
+    swap_token = ERC20Contract.at(token_address);
+
+    for (let i = 0; i < N_COINS; i++) {
+        swap.coins(i, (err, addr) => {
+            coins[i] = ERC20Contract.at(addr);
+            if (i == 0) {
+                from_currency = 0;
+                set_amount(0, "#from_currency");
+            }
+            if (i == 1) {
+                to_currency = 1;
+                set_amount(1, "#to_currency");
+            }
+        });
+    }
+}
+
+function init_ui() {
+    $('input[type=radio][name=from_cur]').change(function() {
+        from_currency = this.value;
+        set_amount(this.value, "#from_currency");
+        if (to_currency == from_currency) {
+            $("#to_cur_" + to_currency).attr('checked', false);
+            if (from_currency == 0) {
+                to_currency = 1;
+            } else {
+                to_currency = 0;
+            }
+            $("#to_cur_" + to_currency).attr('checked', true);
+            set_amount(to_currency, "#to_currency");
+        }
+    });
+    $('input[type=radio][name=to_cur]').change(function() {
+        to_currency = this.value;
+        set_amount(this.value, "#to_currency");
+    });
+
+    $("#from_cur_0").attr('checked', true);
+    $("#to_cur_1").attr('checked', true);
+}
+
+window.addEventListener('load', async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            await ethereum.enable();
+            init_contracts();
+            init_ui();
+        } catch (error) {
+            // ???
+        }
+    }
+});
