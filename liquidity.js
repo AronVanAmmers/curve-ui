25507e8edeb08acf36b6203db99dcc860a29ae8f
Michael Egorov <michwill@gmail.com>
2019-10-27 12:05:42 +0100

    WIP - async talking to web3

--- liquidity.js
+++ liquidity.js
@@ -5,6 +5,37 @@ var sync_balances;
 var balances = new Array(N_COINS);
 
 
+// XXXXXXXXXXXXXXXXXXXXX
+// See https://ethereum.stackexchange.com/a/24238
+const promisify = (inner) =>
+  new Promise((resolve, reject) =>
+    inner((err, res) => {
+      if (err) { reject(err) }
+
+      resolve(res);
+    })
+  );
+
+const proxiedWeb3Handler = {
+  // override getter
+  get: (target, name) => {
+    const inner = target[name];
+    if (inner instanceof Function) {
+      // Return a function with the callback already set.
+      return (...args) => promisify(cb => inner(...args, cb));
+    } else if (typeof inner === 'object') {
+      // wrap inner web3 stuff
+      return new Proxy(inner, proxiedWeb3Handler);
+    } else {
+      return inner;
+    }
+  },
+};
+
+const w3 = new Proxy(web3, proxiedWeb3Handler);
+// XXXXXXXXXXXXXXXXXXXXX
+
+
 function set_amount(i, el_id) {
     coins[i].balanceOf(web3.eth.accounts[0], (err, value) => {
         var out = (value / 1e18).toFixed(18);
@@ -18,17 +49,16 @@ function handle_add_liquidity() {
 function handle_remove_liquidity() {
 }
 
-function init_contracts() {
+async function init_contracts() {
     var SwapContract = web3.eth.contract(swap_abi);
     ERC20Contract = web3.eth.contract(ERC20_abi);
 
-    swap = SwapContract.at(swap_address);
-    swap_token = ERC20Contract.at(token_address);
+    swap = new Proxy(SwapContract.at(swap_address), proxiedWeb3Handler);
+    swap_token = new Proxy(ERC20Contract.at(token_address), proxiedWeb3Handler);
 
     for (let i = 0; i < N_COINS; i++) {
-        swap.coins(i, (err, addr) => {
-            coins[i] = ERC20Contract.at(addr);
-        });
+        var addr = await swap.coins(i);
+        coins[i] = new Proxy(ERC20Contract.at(addr), proxiedWeb3Handler);
     }
 
     $("#add-liquidity").click(handle_add_liquidity);
