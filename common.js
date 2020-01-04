18a4b07c0bda802ec791be85235cd2cea894ea8f
Michael Egorov <michwill@gmail.com>
2020-01-04 23:38:37 +0300

    Show my share

--- common.js
+++ common.js
@@ -101,4 +101,16 @@ async function update_fee_info() {
     admin_fee = parseInt(await swap.methods.admin_fee().call()) / 1e10;
     $('#fee-info').text((fee * 100).toFixed(3));
     $('#admin-fee-info').text((admin_fee * 100).toFixed(3));
+
+    var default_account = (await web3.eth.getAccounts())[0];
+    var token_balance = parseInt(await swap_token.methods.balanceOf(default_account).call());
+    if (token_balance > 0) {
+        var token_supply = parseInt(await swap_token.methods.totalSupply().call());
+        var l_info = $('#lp-info li span');
+        $('#lp-info-container').show();
+        for (let i=0; i < N_COINS; i++) {
+            var val = (balances[i] * c_rates[i] * token_balance / token_supply).toFixed(2);
+            $(l_info[i]).text(val);
+        }
+    }
 }
